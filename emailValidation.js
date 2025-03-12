d94af7cdad6b8e1879597e03f6aea862edb19af8
ErmiyasTesfaye <ermiyastesfaye16@gmail.com>
2025-03-13 00:04:08 +0300

    Made email validation feature

---
+++ emailValidation.js
@@ -0,0 +1,7 @@
+function isValidEmail(email) {
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return emailRegex.test(email);
+}
+
+console.log(isValidEmail("test@example.com"));
+console.log(isValidEmail("invalid-email"));
